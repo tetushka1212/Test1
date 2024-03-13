c7f1ba79c93454ffeea42867dadad7198d38aeef
iros04004 <iros04004@gmail.com>
2024-03-13 23:46:16 +0300

    Создание отдельной функции для проверки ввода

--- Test1/Test1.cpp
+++ Test1/Test1.cpp
@@ -2,9 +2,44 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
+void InputVar() {
+	string ageInput;
+	int age;
+
+	cout << "How old are you: ";
+	cin >> ageInput;
+
+	age = stoi(ageInput);
+	try
+	{
+		age = stoi(ageInput);
+	}
+	catch (invalid_argument)
+	{
+		// можно вводить только числа
+		cout << "Only numbers are allowed! \n";
+		age = -1;
+	}
+	catch (...)
+	{
+		//Если будет выброшено какое-то исключение, которое не обработано выше, то говорим, что возникла неизвестная ошибка
+		cout << "Unknown error! \n";
+		age = -1;
+		
+	}
+	if (age > 0)
+	{
+		cout << "You are " << age << " years old. \n";
+	}
+	else
+	{
+		cout << "Try again! \n";
+	}
+}
 
 int main() {
-	cout << "dfdffgdf" << endl;
+	InputVar();
 }
